72f30b098cf7c8965c8c196a53d8734e143c1547
Maciej Wiatr <maciekwiatr17@gmail.com>
2025-08-15 20:10:27 +0200

    Remove export for Gate component from branding index file.

--- src/features/branding/components/index.ts
+++ src/features/branding/components/index.ts
@@ -2,4 +2,3 @@ export { default as AnimatedSymbol } from "./AnimatedSymbol";
 export { default as Logo } from "./Logo";
 export { default as Partners } from "./Partners";
 export { default as Symbol } from "./Symbol";
-export { default as Gate } from "./gate";
