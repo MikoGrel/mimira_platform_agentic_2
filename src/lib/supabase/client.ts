eafa01f678e32482f1c09453681f346799a9090c
Maciej Wiatr <maciekwiatr17@gmail.com>
2025-08-26 15:25:30 +0200

    Supabase broke its api, great

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,16 +1,9 @@
 import { Database } from "$/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
-import { SupabaseClient } from "@supabase/supabase-js";
-
-let client: SupabaseClient<Database> | null = null;
 
 export function createClient() {
-  if (client) return client;
-
-  client = createBrowserClient<Database>(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-
-  return client;
 }
