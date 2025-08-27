683ed8377f580d456f80c8590b7ade05ab46d178
Maciej Wiatr <maciekwiatr17@gmail.com>
2025-08-27 15:12:39 +0200

    Try to disable bundle analysis to fix build

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,12 @@
 import type { NextConfig } from "next";
 import lingoCompiler from "lingo.dev/compiler";
-import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {};
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
-export default withBundleAnalyzer(
-  lingoCompiler.next({
-    sourceLocale: "en",
-    targetLocales: ["pl"],
-    models: "lingo.dev",
-    rsc: true,
-    sourceRoot: "src",
-  })(nextConfig)
-);
+export default lingoCompiler.next({
+  sourceLocale: "en",
+  targetLocales: ["pl"],
+  models: "lingo.dev",
+  rsc: true,
+  sourceRoot: "src",
+})(nextConfig);
