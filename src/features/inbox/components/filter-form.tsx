6c83e8432ffbbe7ab1d7a8dab4d1bddf63545f92
Maciej Wiatr <maciekwiatr17@gmail.com>
2025-09-04 15:56:26 +0200

    Temporarily remove sorting

--- src/features/inbox/components/filter-form.tsx
+++ src/features/inbox/components/filter-form.tsx
@@ -3,7 +3,7 @@
 import { Button, DateRangePicker, Select, SelectItem } from "@heroui/react";
 import { Controller } from "react-hook-form";
 import { PolishVoivodeships } from "$/features/i18n/config/poland-config";
-import { sortingOptions, useFilterForm } from "../hooks/use-filter-form";
+import { useFilterForm } from "../hooks/use-filter-form";
 import { FilterFormType } from "..";
 import { cn } from "$/lib/utils";
 
@@ -54,30 +54,6 @@ export function FilterForm({ onFiltered, className }: FilterFormProps) {
               <SelectItem key={voivodeship.name}>
                 {voivodeship.label}
               </SelectItem>
-            ))}
-          </Select>
-        )}
-      />
-      <Controller
-        control={control}
-        name="sortBy"
-        render={({ field, fieldState }) => (
-          <Select
-            selectedKeys={field.value || undefined}
-            onSelectionChange={field.onChange}
-            label="Sort by offers deadline"
-            errorMessage={fieldState.error?.message}
-            renderValue={(items) => {
-              if (items.length === 0) return null;
-              const selectedKey = items[0].key;
-              const selectedOption = sortingOptions.find(
-                (option) => option.key === selectedKey
-              );
-              return selectedOption ? selectedOption.readable : null;
-            }}
-          >
-            {sortingOptions.map((sortBy) => (
-              <SelectItem key={sortBy.key} startContent={sortBy.readable} />
             ))}
           </Select>
         )}
